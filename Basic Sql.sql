45c889ae58e7bc538e0b1f367ff8a023e585ffe3
Abhishek D <89733722+abhishekd358@users.noreply.github.com>
2023-02-03 19:36:11 +0530

    ttttd
    ttt

--- Basic Sql.sql
+++ Basic Sql.sql
@@ -2,12 +2,3 @@
 
 --5 (Arithmetic Operations)
 /*Create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for each order.
-Limit the results to the first 10 orders, and include the id and account_id fields.*/
-
-
-
-/*Write a query that finds the percentage of revenue that comes from poster paper for each order. You will need to use only the columns that end with _usd.
-eb_events table to find all information regarding individuals who were contacted
- via the organic or adwords channels, and started their account at any point in 2016, sorted from newest to oldest.*/
- SELECT *AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') 
-           AND pri
